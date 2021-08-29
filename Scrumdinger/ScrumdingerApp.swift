8f9d086a3883a6da13b1f8dfa8495ee058970dd2
Logan Koshenka <koshenka27@gmail.com>
2021-08-29 13:49:43 -0400

    Initial Commit

---
+++ Scrumdinger/ScrumdingerApp.swift
@@ -0,0 +1,17 @@
+//
+//  ScrumdingerApp.swift
+//  Scrumdinger
+//
+//  Created by Logan Koshenka on 8/29/21.
+//
+
+import SwiftUI
+
+@main
+struct ScrumdingerApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
